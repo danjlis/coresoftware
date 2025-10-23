c973de2eec5c404783e77aaaff89d8828a47bbbe
Daniel J Lis <dlis@sphnxuser07.sdcc.bnl.gov>
2025-10-23 13:03:09 -0400

    calovertexmap first commit

--- offline/packages/globalvertex/GlobalVertex.h
+++ offline/packages/globalvertex/GlobalVertex.h
@@ -22,7 +22,9 @@ class GlobalVertex : public PHObject
     SMEARED = 200,
     MBD = 300,
     SVTX = 400,
-    SVTX_MBD = 500
+    SVTX_MBD = 500,
+    CALO = 600,
+    MBD_CALO = 700
   };
 
   typedef std::vector<const Vertex*> VertexVector;
